1d1a219d2a616f2481cb7501f5eba569e989399a
shgtkshruch <shgtk.shruch@gmail.com>
2014-09-24 11:16:12 +0900

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+(function() {
+  var drow_canvas;
+
+  window.onload = function() {
+    return drow_canvas();
+  };
+
+  drow_canvas = function() {
+    var Circle, canvas, ch, circles, ctx, cw, draw, i, num, _i;
+    canvas = document.getElementById('c');
+    ctx = canvas.getContext('2d');
+    cw = canvas.width = window.innerWidth;
+    ch = canvas.height = window.innerHeight;
+    circles = [];
+    num = cw / 2;
+    Circle = (function() {
+      function Circle() {
+        this.x = Math.random() * cw;
+        this.y = ch + Math.random() * 100;
+        this.r = 1 + Math.random() * 5;
+        this.alpha = 0.2 + Math.random() * 0.3;
+        this.velocity = Math.random();
+      }
+
+      Circle.prototype.draw = function() {
+        this.alpha -= 0.0005;
+        if (this.alpha < 0) {
+          this.constructor();
+        }
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        ctx.fillStyle = 'rgba(255,255,255,' + this.alpha + ')';
+        ctx.fill();
+        return this.y -= this.velocity;
+      };
+
+      return Circle;
+
+    })();
+    for (i = _i = 0; 0 <= num ? _i < num : _i > num; i = 0 <= num ? ++_i : --_i) {
+      circles.push(new Circle());
+    }
+    return (draw = function() {
+      ctx.clearRect(0, 0, cw, ch);
+      circles.forEach(function(c) {
+        return c.draw();
+      });
+      return requestAnimationFrame(draw);
+    })();
+  };
+
+}).call(this);
